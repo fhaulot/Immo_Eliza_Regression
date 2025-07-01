838bd37ad144d695afc1341ecf86d43ec2c2d593
fhaulot <fhaulot@gmail.com>
2025-07-01 11:52:01 +0200

    updating preprocessed and now working on pipeline

--- Floriane/Main.py
+++ Floriane/Main.py
@@ -1,14 +1,13 @@
 from pipeline import Preprocessing
 import pandas as pd
-df = pd.read_csv('./Dogs Data/Cleaned_data.csv')
+
 
 preprocessing = Preprocessing()
+df = preprocessing.read_csv(None, r"C:\Users\fhaul\Documents\GitHub\Immo_Eliza_Regression\Dogs Data\cleaned_data.csv")    
 df = preprocessing.drop_columns(df)
-df.info()
-
 df = preprocessing.ordinal_building_condition(df)
-df.info()
-
 df = preprocessing.ordinal_epc_score(df)
 df = preprocessing.categorical_encode(df)
-df.info()
+df = preprocessing.postcode_geo(df)
+df.head(5)
+df.to_csv(r"C:\Users\fhaul\Documents\GitHub\Immo_Eliza_Regression\Dogs Data\preprocessed_data.csv", index=False)
--- Floriane/pipeline.py
+++ Floriane/pipeline.py
@@ -6,7 +6,6 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-
 class Preprocessing : 
 
     def __init__(self):
@@ -24,34 +23,43 @@ class Preprocessing :
         """
         Drop specified columns from the DataFrame.
         """
-        Col_to_drop = ["url", 'locality', "MunicipalityCleanName"]
+        Col_to_drop = ["id", "url", 'locality', "MunicipalityCleanName", "province", "region", "price_square_meter"]
         cleaned_columns = df.drop(columns=Col_to_drop, axis=0)
         print(cleaned_columns.info())
         return cleaned_columns
     
     def ordinal_building_condition(self, df):
-        """
-        Perform ordinal encoding on the building condition using a mapping.
-        """
-        df = pd.DataFrame({"buildingCondition" : ["AS_NEW", "GOOD", "JUST_RENOVATED", "TO_RENOVATE", "TO_BE_DONE_UP", "TO_RESTORE"]})
-        condition_order = {"AS_NEW": 1, "GOOD": 2, "JUST_RENOVATED": 3, "TO_RENOVATE": 4, "TO_BE_DONE_UP": 5, "TO_RESTORE": 6}
+        condition_order = {"AS_NEW": 1, "GOOD": 2, "JUST_RENOVATED": 3,
+        "TO_RENOVATE": 4, "TO_BE_DONE_UP": 5, "TO_RESTORE": 6}
         df['encoded_condition'] = df['buildingCondition'].map(condition_order)
+        df.drop(columns=['buildingCondition'], inplace=True)
         return df
     
     def ordinal_epc_score(self, df) :
         """
         Perform ordinal encoding for the column of epc score using a mapping
         """
-        df = pd.DataFrame({"epcScore" : ["A", "B", "C", "D", "E", "F", "G"]})
         epc_order = {"A" : 1, "B" : 2, "C" : 3, "D" : 4, "E" : 5, "F" : 6, "G" : 7}
         df['encoded_epc'] = df['epcScore'].map(epc_order)
+        df.drop(columns=['epcScore'], inplace=True)
+        return df
     
     def categorical_encode(self, df):
         """
         Perform categorical encoding on specified columns.
         """
-        encoder = OneHotEncoder(sparse_output=False).set_output(transform="pandas")
-        for cat_par in (["type", "subtype", "province", "region"]):
-            new_cols_df = encoder.fit_transform(df[cat_par])
-            df = pd.concat([df, new_cols_df], axis=1).drop([cat_par], axis=1)
+        df_encoded = pd.get_dummies(df, columns=["type", "subtype"], drop_first=True)
+        bool_cols = df_encoded.select_dtypes(include=["bool"]).columns
+        df_encoded[bool_cols] = df_encoded[bool_cols].astype(int)
+        return df_encoded
+    
+    def postcode_geo(self, df):
+        """
+        Add geographical coordinates (latitude and longitude) to the DataFrame based on postcodes.
+        """
+        df_postcode = pd.read_csv(r'C:\Users\fhaul\Documents\GitHub\Immo_Eliza_Regression\Floriane\belgian-cities-geocoded (1).csv')
+        df_postcode = df_postcode.astype({'postCode': 'int64'})
+        # df_postcode.info()
+        df = pd.merge(df, df_postcode[['postCode', 'lat', 'lng']], on='postCode', how='left')
+        df = df.drop(columns=["postCode"], axis=1)
         return df
